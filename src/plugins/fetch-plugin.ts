c9f8c8c2e924a7122a923a7112ca7a5f5403e2a9
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-10-04 23:03:42 +0500

    Extracting Common Caching Logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -18,13 +18,15 @@ export const fetchPlugin = (inputCode: string) => {
       };
     });
 
-    build.onLoad({ filter: /.css$/ }, async (args: any) => {
+    build.onLoad({ filter: /.*/ }, async (args: any) => {
       const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
-      if(cachedResult) {
+      if (cachedResult) {
         return cachedResult;
       }
+    });
 
+    build.onLoad({ filter: /.css$/ }, async (args: any) => {
       const { data, request } = await axios.get(args.path);
       const escaped = data
       .replace(/\n/g, '')
@@ -48,15 +50,7 @@ export const fetchPlugin = (inputCode: string) => {
     })
       
     build.onLoad({ filter: /.*/ }, async (args: any) => {
-
-      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-
-      if(cachedResult) {
-        return cachedResult;
-      }
-
       const { data, request } = await axios.get(args.path);
-
       const result: esbuild.OnLoadResult = {
         loader: 'jsx',
         contents: data,
