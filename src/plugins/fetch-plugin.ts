a3974f51905bbe17fdf0fc96f7d036a2923841c5
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-10-04 22:49:35 +0500

    Tricking ESBuild's CSS Handling & Escaping CSS Snippets

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -21,20 +21,32 @@ export const fetchPlugin = (inputCode: string) => {
 
       // Check to see if we have already fetched this file
       // and if it is in the cache
-      const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+      // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
 
       // if it is, return it immediately
-      if(cachedResult) {
-        return cachedResult;
-      }
+      // if(cachedResult) {
+      //   return cachedResult;
+      // }
 
       const { data, request } = await axios.get(args.path);
 
-      const loader = args.path.match(/.css$/) ? 'css' : 'jsx';
+      const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+      const escaped = data
+      .replace(/\n/g, '')
+      .replace(/"/g, '\\"')
+      .replace(/'/g, "\\'");
+
+      const contents = fileType === 'css' ? 
+        `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        ` : data;
 
       const result: esbuild.OnLoadResult = {
-        loader: loader,
-        contents: data,
+        loader: 'jsx',
+        contents,
         resolveDir: new URL('./', request.responseURL).pathname
       }
       // store response in cache
