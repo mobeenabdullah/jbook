c12e253097fdf430249a28c7f6c18d4139d679b4
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-10-11 10:31:01 +0500

    Automatic Code Formatting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
@@ -8,7 +9,10 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({onChange, initialValue}) => {
+  const editorRef = useRef<any>();
+
   const onEditorDidMount: EditorDidMount = (getValue, monacoEditor) => {
+    editorRef.current = monacoEditor;
     monacoEditor.onDidChangeModelContent(() => {
       onChange(getValue());
     });
@@ -18,10 +22,19 @@ const CodeEditor: React.FC<CodeEditorProps> = ({onChange, initialValue}) => {
 
   const onFormatClick = () => {
     // get current value from editor
+    const unformatted = editorRef.current.getModel().getValue();
 
     // format that value
+    const formatted = prettier.format(unformatted, {
+      parser: "babel",
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    }).replace(/\n$/, "");
 
     // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
   }
 
   return (
