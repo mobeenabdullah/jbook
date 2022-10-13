f595f58289a798aa5fcc5fd678f2f80a7cece41f
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-10-14 02:17:34 +0500

    Update Cell Logic

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -20,13 +20,23 @@ const initialState: CellsState = {
 
 const reducer = (state: CellsState = initialState, action: Action): CellsState => {
   switch (action.type) {
-    case ActionType.MOVE_CELL:
-      return state;
+    case ActionType.UPDATE_CELL:
+      const { id, content } = action.payload;
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [id]: {
+            ...state.data[id],
+            content
+          }
+        }
+      }
     case ActionType.DELETE_CELL:
       return state;
-    case ActionType.INSERT_CELL_BEFORE:
+    case ActionType.MOVE_CELL:
       return state;
-    case ActionType.UPDATE_CELL:
+    case ActionType.INSERT_CELL_BEFORE:
       return state;
     default:
       return state;
