21913d0b5bc88b2543f3b72dbdfc5f706210d601
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-10-15 02:13:34 +0500

    Adding the Action Creator

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,12 +1,17 @@
+import { Dispatch } from "react";
 import { ActionType } from "../action-types";
 import { 
   UpdateCellAction, 
   DeleteCellAction, 
   MoveCellAction, 
   InsertCellAfterAction,
-  Direction
+  BundleStartAction,
+  BundleCompleteAction,
+  Direction,
+  Action
 } from "../actions";
 import { CellTypes } from "../cell";
+import bundle from '../../bundler';
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -44,3 +49,24 @@ export const insertCellAfter = (id: string | null, cellType: CellTypes): InsertC
     }
   };
 };
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId
+      }
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result
+      }
+    });
+  };
+}
