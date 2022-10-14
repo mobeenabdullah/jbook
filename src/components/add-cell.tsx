35ac4aed0f506b2d99597e52eb07d9ab3d60d759
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-10-15 00:13:04 +0500

    Forcing Add Cell Visibility

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -3,14 +3,15 @@ import { useActions } from '../hooks/use-actions';
 
 interface AddCellProps {
   nextCellId: string | null;
+  forceVisible?: boolean;
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId }) => {
+const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
   const { insertCellBefore } = useActions();
 
   return (
     <div>
-      <div className="add-cell">
+      <div className={`add-cell ${forceVisible && 'force-visible'}`}>
         <div className="add-buttons">
           <button className="button is-rounded is-primary is-small" onClick={() => insertCellBefore(nextCellId, 'code')}>
             <span className="icon is-small">
